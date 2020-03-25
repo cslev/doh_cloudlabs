cdeea9ed8fd1aaca5e242ea6969a69e70a355264
cslev <cslev@gmx.com>
2020-03-25 11:55:21 +0800

    fix

--- profile.py
+++ profile.py
@@ -15,27 +15,27 @@ import geni.rspec.emulab as emulab
 import geni.rspec.igext as IG
 
 # Create a portal object
-pc = portal.Context()
-pc.defineParameter(
-    "osNodeType", "Hardware Type",
-    portal.ParameterType.NODETYPE, "utah-m400",
-    [("", "any available type"), "utah-dl360", "utah-d2950", "utah-m510", "utah-xl170", "utah-m400", "utah-r720"],
-    longDescription="http://docs.cloudlab.us/hardware.html A specific hardware type to use for each node.  Cloudlab clusters all have machines of specific types. When you set this field to a value that is a specific hardware type, you will only be able to instantiate this profile on clusters with machines of that type.  If unset, the experiment may have machines of any available type allocated.")
+# pc = portal.Context()
+# pc.defineParameter(
+    # "osNodeType", "Hardware Type",
+    # portal.ParameterType.NODETYPE, "utah-m400",
+    # [("", "any available type"), "utah-dl360", "utah-d2950", "utah-m510", "utah-xl170", "utah-m400", "utah-r720"],
+    # longDescription="http://docs.cloudlab.us/hardware.html A specific hardware type to use for each node.  Cloudlab clusters all have machines of specific types. When you set this field to a value that is a specific hardware type, you will only be able to instantiate this profile on clusters with machines of that type.  If unset, the experiment may have machines of any available type allocated.")
 
 # pc.defineParameter(
 #     "osNodeType", "Hardware Type",
 #     portal.ParameterType.NODETYPE, "wisconsin-c220g1",
 #     [("", "any available type"), "wisconsin-c220g2", "wisconsin-c220g5", "wisconsin-c240g1", "wisconsin-c240g1"],
 #     longDescription="http://docs.cloudlab.us/hardware.html A specific hardware type to use for each node.  Cloudlab clusters all have machines of specific types. When you set this field to a value that is a specific hardware type, you will only be able to instantiate this profile on clusters with machines of that type.  If unset, the experiment may have machines of any available type allocated.")
-params = pc.bindParameters()
+# params = pc.bindParameters()
 # Create a Request object to start building the RSpec.
-request = pc.makeRequestRSpec()
+# request = pc.makeRequestRSpec()
 
 
-tour = IG.Tour()
-tour.Description(IG.Tour.TEXT,kube_description)
-tour.Instructions(IG.Tour.MARKDOWN,kube_instruction)
-request.addTour(tour)
+# tour = IG.Tour()
+# tour.Description(IG.Tour.TEXT,kube_description)
+# tour.Instructions(IG.Tour.MARKDOWN,kube_instruction)
+# request.addTour(tour)
 
 
 doh_resolvers = {'cloudflare':1,
