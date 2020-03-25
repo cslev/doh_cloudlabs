51e1a7b33ec521002341393426f02dfd3d352bbb
cslev <cslev@gmx.com>
2020-03-25 12:01:07 +0800

    fix again

--- profile.py
+++ profile.py
@@ -60,7 +60,7 @@ for key in doh_resolvers:
 
     # node doh_cloudflare
     kube_doh = request.RawPC(str(key))
-    kube_doh.hardware_type = params.osNodeType
+    # kube_doh.hardware_type = params.osNodeType
     # kube_doh.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:DEB8-64-STD' #<-- does not work
     kube_doh.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
     kube_doh.Site(str(key))
