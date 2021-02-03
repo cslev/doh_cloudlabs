cef59d2c008f21071fc2d2c5469e1ccfb5fe1a5f
cslev <cs.lev@gmx.com>
2021-02-03 17:37:02 +0800

    remove architecture

--- profile.py
+++ profile.py
@@ -62,7 +62,7 @@ end=2000
 key = "doh_docker"
 kube_doh = request.RawPC(str(key))
 # kube_doh.hardware_type = "m400" ##ARM
-kube_doh.hardware_type = "xl170" ##AMD64
+#kube_doh.hardware_type = "xl170" ##AMD64
 # kube_doh.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:DEB8-64-STD' #<-- does not work
 kube_doh.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD'
 kube_doh.Site(str(key))
