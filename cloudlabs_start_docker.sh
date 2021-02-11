37487d9c8c4b27f0976d7308925cdbe0df9e9f84
cslev <cs.lev@gmx.com>
2021-02-11 09:44:48 +0800

    add architecture-agnostic docker install script

--- cloudlabs_start_docker.sh
+++ cloudlabs_start_docker.sh
@@ -62,9 +62,10 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends $
 #sudo apt-get dist-upgrade -y --no-install-recommends
 
 #installing docker
+arch=$(dpkg -l |grep linux-image-generic|awk '{print $4}')
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=$arch] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
